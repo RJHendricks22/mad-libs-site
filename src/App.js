59126a73d8698da17978cbea7d994cea688071d5
Raymond Hendricks <rayjhendricks22@gmail.com>
2017-07-11 15:52:47 -0400

    add name, madlib fields on change

--- src/App.js
+++ src/App.js
@@ -3,19 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      madlib: "",
+      name: "",
+      namedisplay: "show"
+    };
+    this.nameChange = this.nameChange.bind(this);
+    this.madLibChange = this.madLibChange.bind(this);
+    this.hideNameInput = this.hideNameInput.bind(this);
+  };
+
+  nameChange(e){
+    this.setState({
+      name: e.target.value
+    });
+  };
+
+  madLibChange(e){
+    this.setState({
+      madlib: e.target.value
+    });
+  };
+  
+  hideNameInput(e){
+    this.setState({
+      namedisplay: "none"
+    });
+    e.preventDefault();
+  };
+
   render() {
+    let nameplate= "";
+
+    if (this.state.name == "") {
+      nameplate = "Type in your name!";
+    } else {
+      nameplate = `Hey ${this.state.name}!`;
+    };
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{nameplate}</h1>
+        <form action="/signup">
+          <div id="nameInput" style={{display: this.state.namedisplay}}>
+            <label for="name">What's your name?</label>
+            <input name="name" type="text" onChange={this.nameChange}/><button onClick={this.hideNameInput}>Accept</button>
+          </div>
+          <br/><br/>
+          <h3>Copy and Paste the mad lib</h3>
+          <textarea name="madlibinput" onChange={this.madLibChange}></textarea>
+          <h2>Here's what you have so far...</h2>
+          <p>{this.state.madlib}</p>
+        </form>
       </div>
     );
   }
-}
+};
 
 export default App;
