4bc333f2a7a0ee83985062badc6daf79ca5c4257
Raymond Hendricks <rayjhendricks22@gmail.com>
2017-08-01 13:08:56 -0400

    Add changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {WordRender} from './WordRender';
+import {MadLibGenerate} from './MadLibGenerate';
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,6 +16,7 @@ class App extends Component {
     this.nameChange = this.nameChange.bind(this);
     this.madLibInput = this.madLibInput.bind(this);
     this.hideNameInput = this.hideNameInput.bind(this);
+    this.renderMadLib = this.renderMadLib.bind(this);
   };
 
   nameChange(e){
@@ -42,12 +44,14 @@ class App extends Component {
   renderMadLib(e){
     e.preventDefault();
     let wordsArray = document.getElementsByClassName('WordRender');
-    let adjectives = [];
-    for (let x=0;x<wordsArray.length;x++){
-      if (wordsArray[x].value != 0){
-        adjectives.push(wordsArray[x])
-      }
-    };
+    console.log( wordsArray )
+    return wordsArray
+//    let changedWords = [];
+//    for (let x=0;x<wordsArray.length;x++){
+//      if (wordsArray[x].value != 0){
+//        changedWords.push(wordsArray[x])
+//      }
+//    };
 //    renderInputFields(adjectives)
   } 
   
@@ -62,11 +66,14 @@ class App extends Component {
     
     let original = {word: ""};
     
+    let madLibStory = {story: [""]};
+    
     let renderInputFields = function(inputArray){ 
       inputArray.map((inputField,i) => 
         <p key={i} >inputField</p>              
       )
-    }
+    };
+
     let madLibOutput = this.state.madlib.map( (word,i) => 
         <WordRender key={`word_${i}`} original={word}/>
     );
@@ -79,6 +86,10 @@ class App extends Component {
             <label >What's your name?</label>
             <input name="name" type="text" onChange={this.nameChange}/><button onClick={this.hideNameInput}>Accept</button>
           </div>
+          <br/><br/>
+          <h2>The madlib will appear here</h2>
+          <MadLibGenerate madLibStory={JSON.stringify(this.wordsArray)} />
+          <p>{this.wordsArray}</p>
           <br/><br/>
           <h3>Copy and Paste the mad lib</h3>
           <textarea name="madlibinput" onChange={this.madLibInput}></textarea>
---
+++ src/MadLibGenerate.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export class MadLibGenerate extends Component {
+  constructor(props){
+    super(props);
+  };
+  
+  
+  
+  
+  render(){
+    console.log(this.props.story)
+//    let storyText;
+//    if (this.props.story != ""){
+//      let storyText = this.props.story.map(function(obje,i) {
+//        let outputString = "";
+//        return outputString += obje.innerText()
+//      });
+//    } else {
+//      storyText = "test"
+//    }
+////    let renderStory = this.props.story;
+//      
+    return(
+    <div>
+      <h1>test: </h1>
+    </div>
+    );
+  }
+};
